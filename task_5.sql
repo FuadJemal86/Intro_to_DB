1919178ef22dfb09b0cf29097a0b993dc6541da9
Fuad Jemal <fuad47722@gmail.com>
2025-06-30 12:06:45 +0300

    task_5

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO Customers (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
